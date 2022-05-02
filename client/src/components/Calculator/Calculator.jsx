6347a840f053f0da0ea9c075144b6f0f20579067
Gonzalo Salinas <gonzalosalinasss.96@gmail.com>
2022-05-02 19:21:34 -0300

    refactor calculator component logic

--- client/src/components/Calculator/Calculator.jsx
+++ client/src/components/Calculator/Calculator.jsx
@@ -4,81 +4,148 @@ import CalculatorButton from "../CalculatorButton/CalculatorButton";
 
 export default function Calculator({ numbers, operators }) {
   const [state, setState] = useState({
-    display: "",
+    msg: "Ingrese el primer número",
+    operator: "",
+    displayNumber: "",
+    firstNumber: null,
   });
 
   const onEqualClick = (event) => {
-    const getResult= new Function(`return function operation (){
-      return ${state.display}
-    }`)();
-    setState({
-      ...state,
-      display:`${getResult()}`
-    })
+    const secondNumber = parseFloat(state.displayNumber);
+    const operations = {
+      "+": () => {
+        setState((prevState) => {
+          return {
+            ...prevState,
+            msg: "Presione el boton C para realizar otra operación",
+            displayNumber: (prevState.firstNumber + secondNumber).toString(),
+          };
+        });
+      },
+      "-": () => {
+        setState((prevState) => {
+          return {
+            ...prevState,
+            msg: "Presione el boton C para realizar otra operación",
+            displayNumber: (prevState.firstNumber - secondNumber).toString(),
+          };
+        });
+      },
+      "*": () => {
+        setState((prevState) => {
+          return {
+            ...prevState,
+            msg: "Presione el boton C para realizar otra operación",
+            displayNumber: (prevState.firstNumber * secondNumber).toString(),
+          };
+        });
+      },
+      "/": () => {
+        secondNumber === 0 && divideValidation();
+        secondNumber !== 0 &&
+          setState((prevState) => {
+            return {
+              ...prevState,
+              msg: "Presione el boton C para realizar otra operación",
+              displayNumber: (prevState.firstNumber / secondNumber).toString(),
+            };
+          });
+      },
+    };
+    const operatorSelected = state.operator;
+
+    state.displayNumber!=="" && state.displayNumber!=="-" && state.firstNumber!==null && operations[operatorSelected]();
   };
 
-  const onOperationButtonClick=(event)=>{
-    setState(prevState=>{
-      return {
-        ...prevState,
-        display:prevState.display+event.target.name,
-      }
-    })
-  }
+  const onOperationButtonClick = (event) => {
+    const numberToSave = parseFloat(state.displayNumber);
+    const operatorSelected = event.target.name;
 
-  const onButtonResetClick=(event)=>{
-    setState(prevState=>{
-      return {
-        ...prevState,
-        display:"",
-      }
-    })
-  }
+    operatorSelected==="-" && state.displayNumber==="" && setState({...state,displayNumber:"-"});
 
-  const onButtonNumberClick=(event)=>{
-    setState(prevState=>{
+    state.displayNumber!=="" && state.displayNumber!=="-" && state.operator==="" && setState((prevState) => {
       return {
         ...prevState,
-        display:prevState.display+event.target.name
-      }
-    })
-  }
+        msg: "Ingrese el segundo número",
+        firstNumber: numberToSave,
+        displayNumber: "",
+        operator: operatorSelected,
+      };
+    });
+  };
+
+  const onButtonResetClick = (event) => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        msg: "Ingrese el primer número",
+        displayNumber: "",
+        operator: "",
+        firstNumber: null,
+      };
+    });
+  };
+
+  const onButtonNumberClick = (event) => {
+    const numberClicked = event.target.name;
+    const concatNumberToDisplay = state.displayNumber + numberClicked;
+    setState((prevState) => {
+      return {
+        ...prevState,
+        displayNumber: concatNumberToDisplay,
+      };
+    });
+  };
+
+  const divideValidation = () => {
+    alert("No se puede dividir por cero!");
+    onButtonResetClick();
+  };
 
   return (
-    <div className="calculator">
-      <div className="calculator-header">
-        <div className="calculator-display">{state.display}</div>
-      </div>
-      <div className="calculator-body">
-        <div className="calculator-numbers">
-          <CalculatorButton
-            opOrNumber={"C"}
-            propClassName="button-reset"
-            onClick={onButtonResetClick}
-            name="C"
-          />
-          {numbers.map((number) => {
-            return (
-              <CalculatorButton
-                opOrNumber={number}
-                propClassName={number === "0" ? "button-cero" : "number-button"}
-                onClick={onButtonNumberClick}
-                name={number}
-              />
-            );
-          })}
+    <div className="container">
+      <div className="msg-calculator">{state.msg}</div>
+      <div className="calculator">
+        <div className="calculator-header">
+          <div className="calculator-display">{state.displayNumber}</div>
         </div>
-        <div className="calculator-operators">
-          {operators.map((op) => {
-            return (
-              <CalculatorButton
-                opOrNumber={op}
-                propClassName="operator-button"
-                onClick={op==="="?onEqualClick:onOperationButtonClick}
-                name={op}
-              />
-            );
-          })}
+        <div className="calculator-body">
+          <div className="calculator-numbers">
+            <CalculatorButton
+              opOrNumber={"C"}
+              propClassName="button-reset"
+              onClick={onButtonResetClick}
+              name="C"
+            />
+            {numbers.map((number) => {
+              return (
+                <CalculatorButton
+                  opOrNumber={number}
+                  propClassName={
+                    number === "0" ? 
+                    "button-cero" : 
+                    number === "." ?
+                    "button-point":
+                    "number-button"
+                  }
+                  onClick={onButtonNumberClick}
+                  name={number}
+                />
+              );
+            })}
+          </div>
+          <div className="calculator-operators">
+            {operators.map((op) => {
+              return (
+                <CalculatorButton
+                  opOrNumber={op}
+                  propClassName="operator-button"
+                  onClick={op === "=" ? onEqualClick : onOperationButtonClick}
+                  name={op}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
